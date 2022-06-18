af6fc5b91389c12f20e56edc29a35219d0982bf9
LhAllant <107412122+LhAllant69@users.noreply.github.com>
2022-06-18 16:44:34 -0400

    Update cli_handler.py

--- cli_handler.py
+++ cli_handler.py
@@ -1,7 +1,7 @@
 import generator
 
 while True:
-    generator.generate(input("How many usernames should be generated"))
+    generator.generate(input("Number of usernames to generate"))
 
     a = input("Generate more ? (y/n)")
     if a.lower() != "y":
